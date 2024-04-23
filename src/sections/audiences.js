839bc4099e615556a37261b5c1d7b55ff2121b77
stretch07 <78528552+stretch07@users.noreply.github.com>
2024-04-22 20:18:03 -0700

    feat: Audiences support

--- src/sections/audiences.js
+++ src/sections/audiences.js
@@ -1,8 +1,72 @@
 import ora from "ora"
 import enquirer from "enquirer";
 
-export default async function({resend, apiKey}) {
+const logAudience = (audience) => {
+    console.log(`- ID: ${audience.id}`)
+    console.log(`- Name: ${audience.name}`)
+    if (audience.created_at) {
+        console.log(`- Created At: ${audience.created_at}`)
+    }
+}
 
+export default async function({resend, apiKey}) {
+    let mainForm = new enquirer.Select({
+        message: "Choose an action",
+        choices: [
+            {name: "add", message: "Add Audience"},
+            {name: "retrieve", message: "List/Retrieve Audience"},
+            {name: "delete", message: "Delete Audience"},
+        ]
+    })
+    mainForm = await mainForm.run()
+
+    switch (mainForm) {
+        case "add":
+            {
+                let form = new enquirer.Form({
+                    name: "createAudience",
+                    message: "Create an audience",
+                    choices: [
+                        {name: "name", message: "Name", initial: "My Audience"},
+                    ]
+                })
+                form = await form.run()
+                const spinner = ora({text: "Creating audience...", spinner: "toggle9"}).start()
+                const audience = await resend.audiences.create({name: form.name})
+                spinner.stop()
+                console.log(`Audience created.`)
+                logAudience(audience.data)
+            }
+            break
+        case "retrieve":
+            {
+                const audienceId = await selectAudience({resend}, "Choose an audience")
+                const spinner = ora({text: "Retrieving audience...", spinner: "toggle9"}).start()
+                const audience = await resend.audiences.get(audienceId)
+                spinner.stop()
+                console.log(`Audience '${audience.data.name}' retrieved.`)
+                logAudience(audience.data)
+            }
+            break
+        case "delete":
+            {
+                const audienceId = await selectAudience({resend}, "Choose an audience")
+                let confirmation = new enquirer.Confirm({
+                    message: "Are you sure you want to delete this audience?",
+                    initial: false
+                })
+                confirmation = await confirmation.run()
+                if (!confirmation) {
+                    console.log("Cancelled.")
+                    process.exit(0)
+                }
+                const spinner = ora({text: "Deleting audience...", spinner: "toggle9"}).start()
+                await resend.audiences.remove(audienceId)
+                spinner.stop()
+                console.log("Audience deleted.")
+            }
+            break
+    }
 }
 
 export async function selectAudience({resend}, message) {
